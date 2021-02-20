2c256c5eeb9896cc69e737b388959f2dd668f417
Torsten Klinger <torsten@snakx.org>
2021-02-20 02:06:58 +0100

    Final apk 0.1.0

---
+++ app/src/main/java/com/github/mobileproxy/MainActivity.java
@@ -0,0 +1,179 @@
+package com.github.mobileproxy;
+
+import android.app.ActivityManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
+import android.provider.Settings;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText portEditText;
+    private Button startButton, stopButton, wifiTetherButton;
+    private TextView proxyStatusTextView, proxyURLTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setContentView(R.layout.activity_main);
+
+        initializeViews();
+
+        initializeListeners();
+
+        // Set focus
+        portEditText.requestFocus();
+
+        // Keep the screen on
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+    }
+
+    private void initializeListeners() {
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!portEditText.getText().toString().matches("\\d+")){
+                    proxyStatusTextView.setText(getString(R.string.enter_the_port));
+                    proxyURLTextView.setText("");
+                    return;
+                }
+                int port = Integer.parseInt(portEditText.getText().toString());
+                String ip = getIPAddress(true);
+                if (ip.trim().startsWith("10.")) {
+                    proxyStatusTextView.setText(getString(R.string.turn_on_tethering));
+                    proxyURLTextView.setText(getString(R.string.connect_wifi));
+                    return;
+                }
+                try {
+                    if (!(new CheckingPortTask().execute(port).get())) {
+                        proxyStatusTextView.setText(getString(R.string.busy_port));
+                        proxyURLTextView.setText(getString(R.string.enter_another_port));
+                        return;
+                    }
+                } catch (ExecutionException | InterruptedException e) {
+                    e.printStackTrace();
+                    proxyStatusTextView.setText(getString(R.string.errors));
+                    proxyURLTextView.setText("");
+                    return;
+                }
+                Intent intent = new Intent(MainActivity.this, ProxyService.class);
+                intent.putExtra("port", port);
+                startService(intent);
+                proxyStatusTextView.setText(getString(R.string.proxy_is_running));
+                proxyURLTextView.setText(String.format("%s:%d", getIPAddress(true), port));
+                startButton.setEnabled(false);
+                stopButton.setEnabled(true);
+            }
+        });
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopService(new Intent(MainActivity.this, ProxyService.class));
+                proxyStatusTextView.setText(getString(R.string.proxy_stopped));
+                proxyURLTextView.setText("");
+                stopButton.setEnabled(false);
+                startButton.setEnabled(true);
+            }
+        });
+        wifiTetherButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchHotspotSettings();
+            }
+        });
+    }
+
+    private void initializeViews() {
+        portEditText = findViewById(R.id.portEditText);
+        startButton = findViewById(R.id.startButton);
+        stopButton = findViewById(R.id.stopButton);
+        wifiTetherButton = findViewById(R.id.WiFiTetherButton);
+        proxyStatusTextView = findViewById(R.id.proxyStatus);
+        proxyURLTextView = findViewById(R.id.proxyURL);
+
+        if (isProxyServiceRunning(ProxyService.class)) {
+            startButton.setEnabled(false);
+            stopButton.setEnabled(true);
+        }
+    }
+
+    public String getIPAddress(boolean useIPv4) {
+        try {
+            boolean isIPv4;
+            List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface intf : interfaces) {
+                List<InetAddress> addrs = Collections.list(intf.getInetAddresses());
+                for (InetAddress addr : addrs) {
+                    if (!addr.isLoopbackAddress()) {
+                        String sAddr = addr.getHostAddress();
+                        isIPv4 = sAddr.indexOf(':')<0;
+
+                        if (useIPv4) {
+                            if (isIPv4)
+                                return sAddr;
+                        } else {
+                            if (!isIPv4) {
+                                int delim = sAddr.indexOf('%'); // drop ip6 zone suffix
+                                return delim<0 ? sAddr.toUpperCase() : sAddr.substring(0, delim).toUpperCase();
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception ignored) { } // for now eat exceptions
+        return "";
+    }
+
+    private void launchHotspotSettings(){
+        Intent tetherSettings = new Intent();
+        tetherSettings.setClassName("com.android.settings", "com.android.settings.TetherSettings");
+        startActivity(tetherSettings);
+    }
+
+    private boolean isProxyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private class CheckingPortTask extends AsyncTask<Integer, Void, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(Integer...port) {
+            try {
+                ServerSocket serverSocket = new ServerSocket(port[0]);
+                serverSocket.close();
+                return true;
+            }
+            catch (Exception e) {
+                return false;
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/github/mobileproxy/ProxyService.java
@@ -0,0 +1,124 @@
+package com.github.mobileproxy;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ProxyService extends Service {
+    final String LOG_TAG = "Proxy";
+    private int port;
+    private ServerThreadTask serverThreadTask;
+
+    public void onCreate() {
+        super.onCreate();
+        Log.d(LOG_TAG, "onCreate");
+    }
+
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(LOG_TAG, "onStartCommand");
+        port = intent.getIntExtra("port", 4333);
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel("com.github.mobileproxy",
+                    "snakx Proxy", NotificationManager.IMPORTANCE_NONE);
+            notificationChannel.setLightColor(Color.BLUE);
+            notificationChannel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+            NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            assert manager != null;
+            manager.createNotificationChannel(notificationChannel);
+        }
+        startForeground(1, new NotificationCompat.Builder(this,
+                "com.github.mobileproxy")
+                .setOngoing(true)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText("Server is running")
+                .setContentIntent(pendingIntent)
+                .build());
+        startProxy();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    public void onDestroy() {
+        serverThreadTask.interrupt();
+        super.onDestroy();
+
+        Log.d(LOG_TAG, "onDestroy");
+    }
+
+    public IBinder onBind(Intent intent) {
+        Log.d(LOG_TAG, "onBind");
+        return null;
+    }
+
+    void startProxy() {
+        serverThreadTask = new ServerThreadTask(port);
+        serverThreadTask.setDaemon(true);
+        serverThreadTask.start();
+        //new ServerTask().execute();
+    }
+
+    class ServerThreadTask extends Thread{
+        private int port;
+
+        public ServerThreadTask(int port) {
+            this.port = port;
+        }
+
+        @Override
+        public void run() {
+            try {
+                ServerSocket httpSocket = new ServerSocket(port);
+                Socket clientSocket;
+                while (!interrupted()) {
+                    clientSocket = httpSocket.accept();
+                    System.out.println("Socket accepted");
+                    new ClientSocketHandler(clientSocket).start();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    class ServerTask extends AsyncTask<Void, Void, Void> {
+        private int port;
+
+        @Override
+        protected void onPreExecute() {
+            port = ProxyService.this.port;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                ServerSocket httpSocket = new ServerSocket(port);
+                Socket clientSocket;
+                while (FlagSingleton.getInstance().isFlag()) {
+                    clientSocket = httpSocket.accept();
+                    System.out.println("Socket accepted");
+                    new ClientSocketHandler(clientSocket).start();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+}
